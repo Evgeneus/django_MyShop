5a347b3a614fbc6723a1857d601c2984a5885909
Evgeny <krivosheevevgeny@gmail.com>
2015-02-04 15:33:34 +0300

    create contacts, delivery, discounts pages

--- article/urls.py
+++ article/urls.py
@@ -7,5 +7,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'mypoject.views.home', name='home'),
     url(r'^articles/get/(?P<product_id>\d+)/$', 'article.views.article'),
     url(r'^articles/get_category/(?P<category_id>\d+)/$', 'article.views.categorys_product_display'),
+    url(r'^contacts/$', 'article.views.get_contacts_page'),
+    url(r'^delivery/$', 'article.views.get_delivery_page'),
+    url(r'^discounts/$', 'article.views.get_discounts_page'),
     url(r'^$', 'article.views.articles'),
     )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- article/views.py
+++ article/views.py
@@ -15,23 +15,48 @@ def custom_proc(request):
 def articles(request):
 	args = {}
 	args['products'] = Product.objects.all()
-	return render_to_response('articles.html',
+	return render_to_response(
+		'articles.html',
 		args,
-		context_instance = RequestContext(request, processors = [custom_proc]))
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
 
 
 def article(request, product_id=1):
 	args = {}
 	args['product'] = Product.objects.get(id = product_id)
 	args['image'] = Image.objects.filter(product__id__contains = product_id)
-	return render_to_response('article.html',
+	return render_to_response(
+		'article.html',
 		args,
-		context_instance = RequestContext(request, processors = [custom_proc]))
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
 
 
 def categorys_product_display(request, category_id = 1):
 	args = {}
 	args['products'] = Product.objects.filter(product_category_id = category_id)
-	return render_to_response('articles.html',
+	return render_to_response(
+		'articles.html',
 		args, 
-		context_instance = RequestContext(request, processors = [custom_proc]))
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
+
+
+def get_contacts_page(request):
+	return render_to_response(
+		'contacts.html',
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
+
+def get_delivery_page(request):
+	return render_to_response(
+		'delivery.html',
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
+
+def get_discounts_page(request):
+	return render_to_response(
+		'discounts.html',
+		context_instance = RequestContext(request, processors = [custom_proc])
+		)
